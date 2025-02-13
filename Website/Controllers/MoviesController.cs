6c2f73ea6670ba9054fc6dd1bde97e2bb861361d
Koen Boone <koen@squintz.be>
2025-02-14 00:11:04 +0100

    Add basis for movies page

---
+++ Website/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using StreamingCheckArr.Core.Models;
+
+namespace StreamingCheckArr.Website.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index(bool getNew = false)
+        {
+            //get the movies from radarr
+            RadarrClient rc = new RadarrClient();
+            List<RadarrMovies> movies = rc.getMovies(getNew).Result.ToList();
+
+            return View(movies);
+        }
+    }
+}
